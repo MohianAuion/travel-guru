d75b7d68e477c3e73a1dc5cfbf481bbd63ebc036
MohianAuion <ahmohian07.gmail.com>
2026-08-19 21:05:53 +0600

    home-page-update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import AuthContext from '../../Context/AuthContext';
 const Navbar = ({light=false}) => {
 
   const{user, logOut}=use(AuthContext);
+ 
 
     const links=<div className={`flex gap-7 font-medium ${light? 'text-black' : 'text-white'}`}>
         <li><NavLink>News</NavLink></li>
@@ -28,31 +29,40 @@ const Navbar = ({light=false}) => {
     return (
         <div className="max-w-10/12 mx-auto rounded-md">
   <div className="collapse-title navbar flex justify-between pt-5 ">
+    {/* first div */}
     <div >
       <img className={`w-34 ${light?'':'brightness-0 invert'}`} src={logo} alt="" />
     </div>
 
-   <div className='flex items-center gap-10 '>
+{/* 2nd div */}
+   <div className='flex items-center gap-10'>
 
     {/* search */}
-    {
+   <div>
+     {
       light?<div></div> : <div className='relative'>
       <input type="text" placeholder="Search Your Destination" className="input pr-16 pl-9 w-64 lg:w-auto text-white font-semibold bg-gray-300/50" />
       <span className='absolute text-white top-3 right-63'><FaSearch></FaSearch></span>
     </div>
     }
 
+   </div>
 {/* links */}
-    <div className=''>
-      <ul className="menu menu-horizontal px-1 ">
+   
+   <div>
+       <ul className="menu menu-horizontal px-1 ">
         {
             links
         }
       </ul>
-      {
+   </div>
+  
+
+     {/* button */}
+    <div>
+       {
         user? <Link onClick={handleLogOut} to='/auth' className='btn btn-warning ml-7'>LogOut</Link>: <Link to='/auth' className='btn btn-warning ml-7'>LogIn</Link>
       }
-      
     </div>
    </div>
     
